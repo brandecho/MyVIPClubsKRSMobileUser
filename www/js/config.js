75d316b33af7237ed9cd06f7221a760e75cc497e
brandecho <pvazquez@brandechomedia.com>
2016-09-25 12:14:56 -0400

    Update 9/25/2016

--- www/js/config.js
+++ www/js/config.js
@@ -1,8 +1,8 @@
 
-var krms_config ={	
+var krms_config ={
 	'ApiUrl' : "http://www.myvipclubs.com/shop/mobileapp/api",
 	'DialogDefaultTitle' : "MyVIPClubs Says",
 	'pushNotificationSenderid' : "30879657255",
 	'facebookAppId' : "235939103087629",
-	'APIHasKey' : "c4ec0c2db3ef373b7937bf3764c5f680"
+	// 'APIHasKey' : "c4ec0c2db3ef373b7937bf3764c5f680"
 };
